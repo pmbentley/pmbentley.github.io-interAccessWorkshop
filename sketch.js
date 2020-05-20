03c4bd6027ce82cdf5d891b22646c1726044c5cb
pmbentley <bentley.pbentley@gmail.com>
2020-05-20 10:53:09 -0400

    new files added

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
+function changeImage(){
+
+	document.getElementById('clickHere').src="images/3.jpg";
+}
